b7ab29f84f0b7eeb9587a39cf8afcfd47cb53e72
RichieRJ <richie.jallorina10@gmail.com>
2022-10-07 11:35:19 +1100

    Added UR3 and kuka models

--- Lab_2.m
+++ Lab_2.m
@@ -1,4 +1,17 @@
 clf; clear; close all; clc;
 
-%% Test Kuka LBR iiwa 14 R820 Robot
-robot = LBRiiwa14R820;
+%% Setting up UR3 and Kuka LBR iiwa 14 R820 Robot
+UR3 = LinearUR3(false);
+hold on
+kuka = LBRiiwa14R820;
+
+kuka.model.base = kuka.model.base * transl(-1,1.3,0);
+startQ = zeros(1,7);
+UR3.model.animate(startQ)
+kuka.model.animate(startQ)
+
+%% Setting up grippers
+% UR3 gripper (suction)
+
+
+% kuka gripper
