f0bd4bcae1035fffb0e5377bc7614835802b62ab
RichieRJ <richie.jallorina10@gmail.com>
2022-10-21 14:21:51 +1100

    Adjusted environment

--- Lab_2.m
+++ Lab_2.m
@@ -10,22 +10,25 @@ axis equal;
 axis on;
 % view(3);
 
-%% Loading Conveyor Belt
+%% Loading the environment
 PlaceObject('Conveyor_Belt.ply');
+beam = placeply('beams_low.ply',0,0.3,0,0);
+Table = placeply('Table.ply',0.5,0,0,pi/2);
+Table2 = placeply('Table.ply',0.9,0,0,pi/2);
+conveyor2 = placeply('Conveyor_Belt.ply',0,0,0,pi);
 
-%% Loading Table
-PlaceObject('Table.ply');
-
-%% Loading Beam
-PlaceObject('Beams.ply');
 
 %% Setting up UR3 and Kuka LBR iiwa 14 R820 Robot
-UR3 = LinearUR3(false);
-UR3_startQ = [0,0,-pi/2,0,-pi/2,0,0];
-UR3.model.animate(UR3_startQ)
+ur3 = LinearUR3(false);
+ur3_startQ = zeros(1,7);
+ur3.model.animate(ur3_startQ)
 % Desired Roll = 180, Pitch = 0 (facing down)
 hold on
-KUKA = LBRiiwa14R820;
+kuka = LBRiiwa14R820;
+kuka_startQ = [0 -pi/2 0 pi/2 0 0 0];
+kuka.model.base = transl(0,0,0.9)*trotx(pi);
+kuka.model.animate(kuka_startQ)
+
 
 % startQ = zeros(1,7);
 % UR3.model.animate(startQ)
@@ -35,11 +38,11 @@ KUKA = LBRiiwa14R820;
 % UR3 gripper (suction)
 workspace = [-0.5 0.5 -0.5 0.5 0 0.5];
 L1 = Link('d',0,'a',0,'alpha',0, 'offset',0);
-UR3Grip = SerialLink([L1],'name','UR3Grip');
-UR3Grip.base = UR3.model.fkine(UR3_startQ) * trotx(pi) * transl(0,0,-0.11);
+ur3Grip = SerialLink([L1],'name','ur3Grip');
+ur3Grip.base = ur3.model.fkine(ur3_startQ) * trotx(pi) * transl(0,0,-0.11);
 [faceData,vertexData] = plyread('vacuumGripper.ply','tri');
-UR3Grip.faces = {faceData, []};
-UR3Grip.points = {vertexData, []};
-plot3d(UR3Grip,0,'workspace',workspace);
+ur3Grip.faces = {faceData, []};
+ur3Grip.points = {vertexData, []};
+plot3d(ur3Grip,0,'workspace',workspace);
 
 % kuka gripper
